df75bf523577e5e54304bc38f2b55ff3a2aba05c
zheng xiang <1767765896@qq.com>
2026-08-10 15:50:26 +0800

    Initial commit

---
+++ src/main/java/com/example/barstorybackend/BarstoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.barstorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarstoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarstoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barstorybackend/BarstoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barstorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarstoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
