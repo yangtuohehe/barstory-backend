ecbc3bca33c80b353b9aa73bdfb3002e571d7a99
zheng xiang <1767765896@qq.com>
2026-08-11 11:39:45 +0800

    数据库连接不上

--- src/main/java/com/example/barstorybackend/BarstoryBackendApplication.java
+++ src/main/java/com/example/barstorybackend/BarstoryBackendApplication.java
@@ -1,11 +1,10 @@
 package com.example.barstorybackend;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.barstorybackend.mapper")
 public class BarstoryBackendApplication {
 
     public static void main(String[] args) {
